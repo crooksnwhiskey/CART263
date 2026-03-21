a201f67322c1f9798ad719b877ec82f86971a997
crooksnwhiskey <alex.chardon36@gmail.com>
2026-03-21 19:25:05 -0400

    finished task 6

--- classwork/CART263-TASK-6/js/RectangularObj.js
+++ classwork/CART263-TASK-6/js/RectangularObj.js
@@ -15,15 +15,20 @@ class RectangularObj {
 
   display() {
     this.context.fillStyle = this.fill_color; // change the color we are using
-    this.context.fillRect(this.x, this.y,this.width, this.height);
+    this.context.fillRect(this.x, this.y, this.width, this.height);
     this.context.strokeStyle = this.stroke_color; // change the color we are using
     this.context.lineWidth = 2; //change stroke
-    this.context.strokeRect(this.x, this.y,this.width, this.height);
+    this.context.strokeRect(this.x, this.y, this.width, this.height);
   }
 
-  update(){
+  update() {
     //update freestyle
-   // this.x+=1;
+    // this.x+=1;
+    let volume = micVolume;
+    let newvolume = Math.min(volume * 5, 255);
+    this.fill_color = `rgb(${newvolume}, ${newvolume / 2}, ${50})`;
+    this.width = 10 + micFrequency * 2;
+    this.y = 100 + Math.sin(Date.now() * 0.002) * 30;
     //console.log("rectangle update")
-}
+  }
 }
